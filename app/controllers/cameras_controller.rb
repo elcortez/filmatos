3da0feab323a09539751d5125f542c0c35344b60
carole danancher <carole.danancher@edhec.com>
2016-05-18 13:11:14 +0200

    map on side is done

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -1,5 +1,7 @@
 class CamerasController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+  layout "map", only: [:index]
+
   def index
     load_cameras
     @categories = Camera.categories
