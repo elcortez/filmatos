8f6242e2ce315091927a23c3f2a5812b7a5c18c1
Quentin Roquigny <roquignyq@gmail.com>
2016-05-20 11:26:49 +0200

    search bar works if empty

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -25,17 +25,21 @@ class Camera < ActiveRecord::Base
 
 
   def available?(user_start_date, user_end_date)
-    user_start_date = Date.new(("20" + user_start_date[6..-1]).to_i, user_start_date[3..4].to_i, user_start_date[0..1].to_i)
-    user_end_date = Date.new(("20" + user_end_date[6..-1]).to_i, user_end_date[3..4].to_i, user_end_date[0..1].to_i)
-    self.bookings.each do |booking|
-      booking_start_date = Date.new(("20" + booking.start_date[6..-1]).to_i, booking.start_date[3..4].to_i, booking.start_date[0..1].to_i)
-      booking_end_date = Date.new(("20" + booking.end_date[6..-1]).to_i, booking.end_date[3..4].to_i, booking.end_date[0..1].to_i)
-      dates_range = (booking_start_date..booking_end_date)
-      return false if dates_range.include?(user_start_date)
-      return false if dates_range.include?(user_end_date)
-      return false if user_start_date < booking_start_date && user_end_date > booking_end_date
+    if user_start_date.blank? || user_end_date.blank?
+      return true
+    else
+      user_start_date = Date.new(("20" + user_start_date[6..-1]).to_i, user_start_date[3..4].to_i, user_start_date[0..1].to_i)
+      user_end_date = Date.new(("20" + user_end_date[6..-1]).to_i, user_end_date[3..4].to_i, user_end_date[0..1].to_i)
+      self.bookings.each do |booking|
+        booking_start_date = Date.new(("20" + booking.start_date[6..-1]).to_i, booking.start_date[3..4].to_i, booking.start_date[0..1].to_i)
+        booking_end_date = Date.new(("20" + booking.end_date[6..-1]).to_i, booking.end_date[3..4].to_i, booking.end_date[0..1].to_i)
+        dates_range = (booking_start_date..booking_end_date)
+        return false if dates_range.include?(user_start_date)
+        return false if dates_range.include?(user_end_date)
+        return false if user_start_date < booking_start_date && user_end_date > booking_end_date
+      end
+      return true
     end
-    return true
   end
 end
 
